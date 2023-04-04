2b709443fe79a39fd4d579fdd88cbc6a92742750
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-04 14:26:45 -0400

    Update CloudConnector.ps1

--- CloudConnector.ps1
+++ CloudConnector.ps1
@@ -12,4 +12,22 @@ Function Check-ServiceStatus {
     }
 }
 
-Get-Service -DisplayName "Citrix*" | Select-Object -Property DisplayName, Status, Name
+Check-ServiceStatus("cdfCaptureService")
+Check-ServiceStatus("Citrix Cloud Connector Metrics Service")
+Check-ServiceStatus("Citrix NetScaler Cloud Gateway")
+Check-ServiceStatus("CitrixClxMtpService")
+Check-ServiceStatus("CitrixConfigSyncService")
+Check-ServiceStatus("CitrixHighAvailabilityService")
+Check-ServiceStatus("CitrixITSMAdapterProvider")
+Check-ServiceStatus("CitrixWEMAuthSvc")
+Check-ServiceStatus("CitrixWemMsgSvc")
+Check-ServiceStatus("CitrixWorkspaceCloudADProvider")
+Check-ServiceStatus("CitrixWorkspaceCloudAgentDiscovery")
+Check-ServiceStatus("CitrixWorkspaceCloudAgentLogger")
+Check-ServiceStatus("CitrixWorkspaceCloudAgentSystem")
+Check-ServiceStatus("CitrixWorkspaceCloudAgentWatchdog")
+Check-ServiceStatus("CitrixWorkspaceCloudCredentialProvider")
+Check-ServiceStatus("CitrixWorkspaceCloudWebRelayProvider")
+Check-ServiceStatus("RemoteHCLServer")
+Check-ServiceStatus("XaXdCloudProxy")
+
