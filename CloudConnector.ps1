96bcaf371afb61dd1fe78bc8faf8dee53a33f29b
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-11 14:48:08 -0400

    Formatting update

--- CloudConnector.ps1
+++ CloudConnector.ps1
@@ -1,7 +1,7 @@
 ###
 # This tool is used to gather state information about Citrix Cloud Connectors
 #
-# Updated - 2023-04-11
+# Updated    - 2023-04-11
 # Updated By - MW 
 ###
 
@@ -32,74 +32,92 @@ Function Check-ServiceStatus {
             "cdfCaptureService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\CdfCaptureService\CdfCaptureService.exe"
                 Write-Host "Description: Captures CDF traces from all configured components and components"
+                Write-Host "`n"
              }
             "Citrix Cloud Connector Metrics Service" { 
                 Write-Host "File Location: C:\Program Files(x86)\Citrix\NetScaler Cloud Gateway\MetricsService.exe"
                 Write-Host "Description: Responsible for collection of metrics from the NetScaler Gateway Service as well as for the generation of Synthetics. This service forwards all the data from the Cloud Connector to the Analytics Service"
+                Write-Host "`n"
             }
             "Citrix NetScaler Cloud Gateway" { 
                 Write-Host "File Location: C:\Program Files(x86)\Citrix\NetScaler Cloud Gateway\Citrix.NetScaler.CloudGateway.exe"
                 Write-Host "Description: Provides internet connectivity to on-premise desktops and applications without the need to open in-bound firewall rules or deploying components in the DMZ"
+                Write-Host "`n"
             }
             "CitrixClxMtpService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\ClxMtpService\Citrix.ClxMtpService.exe"
                 Write-Host "Description: ClxMtp Service"
+                Write-Host "`n"
             }
             "CitrixConfigSyncService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\ConfigSync\ConfigSyncService.exe"
                 Write-Host "Description: Copies brokering configuration locally for high availability mode"
+                Write-Host "`n"
             } 
             "CitrixHighAvailabilityService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\Broker\Service\HighAvailabilityService.exe"
                 Write-Host "Description: Provides continuity of service during outage of central site"
+                Write-Host "`n"
             }
             "CitrixITSMAdapterProvider" {
                 Write-Host "File Location: C:\Program Files\Citrix\CitrixITSMAdapterProvider\WorkspaceAutomationConnectorPlugin.exe"
                 Write-Host "Description: Automate provisioning and management of Virtual Apps and Desktops"
+                Write-Host "`n"
              }
             "CitrixWEMAuthSvc" {
                 Write-Host "File Location: C:\Program Files\Citrix\WemProvider\Connector.Authentication.Host.exe"
                 Write-Host "Description: Provides authentication service for Citrix WEM Agents to connect to cloud infrastructure servers"
+                Write-Host "`n"
              }
             "CitrixWemMsgSvc" {
                 Write-Host "File Location: C:\Program Files\Citrix\WemProvider\Connector.Messaging.Host.exe"
                 Write-Host "Description: Provides service for Citrix WEM Cloud service to receive messages from cloud infrastructure servers"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudADProvider" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\Agent\Citrix.CloudServices.Agent.exe"
                 Write-Host "Description: Enabled the Citrix Cloud to facilitate management of resources associated with the Active Directory domain accounts it is installed into"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudAgentDiscovery" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\Agent Discovery\Citrix.CloudServices.AgentDiscovery.exe"
                 Write-Host "Description: Enables the Cloud to faciliate management of XenApp and XenDesktop (Legacy)"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudAgentLogger" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\AgentLogger\Citrix.CloudServices.AgentLogger.exe"
                 Write-Host "Description: Enables the Cloud to faciliate management of XenApp and XenDesktop (Legacy)"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudAgentSystem" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\AgentSystem\Citrix.CloudServices.AgentSystem.exe"
                 Write-Host "Description: Handles the system calls necessary for the on-premise agents"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudAgentWatchdog" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\AgentWatchDog\Citrix.CloudServices.AgentWatchDog.exe"
                 Write-Host "Description: Monitors and upgrades the on-premise agents"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudCredentialProvider" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\CredentialProvider\Citrix.CloudServices.CredentialProvider.exe"
                 Write-Host "Description: Citrix Cloud credential provider"
+                Write-Host "`n"
              }
             "CitrixWorkspaceCloudWebRelayProvider" { 
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\WebRelayAgent\Citrix.CloudServices.WebRelay.Agent.exe"
                 Write-Host "Description: Enables HTTP Requests received from WebRelay Cloud service to be forwarded to on-premises Web Servers"
+                Write-Host "`n"
             }
             "RemoteHCLServer" {
                 Write-Host "File Location: C:\Program Files\Citrix\RemoteHCLServer\Service\RemoteHCLServer.exe"
                 Write-Host "Description: Proxies communication between the Delivery Controller and the Hypervisor"
+                Write-Host "`n"
              }
             "XaXdCloudProxy" { 
                 Write-Host "File Location: C:\Program Files\Citrix\XaXdCloudProxy\XaXdCloudProxy.exe"
                 Write-Host "Description: Enables communication to a remote Broker service from local VDAs and StoreFront servers"
+                Write-Host "`n"
             }
             Default {
                 Write-Host "Something went wrong"
@@ -137,6 +155,7 @@ function RegistryOutput {
 # Checks the status of Citrix servics
 function serviceCheck {
     # Check Service
+    Write-Host "`n# Service Status" -ForegroundColor Yellow
     Check-ServiceStatus("cdfCaptureService")
     Check-ServiceStatus("Citrix Cloud Connector Metrics Service")
     Check-ServiceStatus("Citrix NetScaler Cloud Gateway")
@@ -160,7 +179,7 @@ function serviceCheck {
 
 # Gets LHC Information
 function LHCStatus {
-    Write-Host "`n# LHC Status"
+    Write-Host "`n# LHC Status" -ForegroundColor Yellow
     RegistryOutput -Path "HKLM:\Software\Citrix\Broker\Service\State\LHC" -Name "Enabled" -ExpectedValue 1 -Title "LHC Enabled"
     RegistryOutput -Path "HKLM:\Software\Citrix\Broker\Service\State\LHC" -Name "IsElected" -ExpectedValue 1 -Title "LHC Elected Node"
     RegistryOutput -Path "HKLM:\Software\Citrix\Broker\Service\State\LHC" -Name "OutageModeEntered" -ExpectedValue 0 -Title "LHC In Outage Mode"
@@ -168,7 +187,7 @@ function LHCStatus {
 
 # Gets Agent information
 function agentStatus {
-    Write-Host "`n# Agent Status"
+    Write-Host "`n# Agent Status" -ForegroundColor Yellow
     RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\Install\AgentSystem" -Name "ProductVersionBase" -ExpectedValue "6.70.0" -Title "Agent Version"
     RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "Configured" -ExpectedValue 1 -Title "Agent Configured"
     RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ImmediateUpgrade" -ExpectedValue 0 -Title "Agent Upgrade Pending"
@@ -178,7 +197,7 @@ function agentStatus {
 
 # Performs a ping test against the Service Address
 function pingCheck {
-    Write-Host "`n# Ping Test"
+    Write-Host "`n# Ping Test" -ForegroundColor Yellow
     if (Test-Connection (Get-ItemPropertyValue -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormat")) {
         RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormat" -ExpectedValue "https://service-us.citrixworkspaceapi.net/" -Title "Connection Status"
     } else {
@@ -194,7 +213,7 @@ function pingCheck {
 # Gets windows events 
 function getEvents {
     $yesterday = (Get-Date) - (New-TimeSpan -Days 1)
-    Write-Host "`n---Citrix Application Events - FATAL / ERROR / WARNING---"
+    Write-Host "`n--- Citrix Application Events - FATAL / ERROR / WARNING ---" -ForegroundColor Yellow
     Get-WinEvent -FilterHashtable @{LogName='Application'; ProviderName='Citrix*'; Level=1,2,3; StartTime=$yesterday} | Format-List
 }
 
@@ -210,7 +229,7 @@ function main {
     if ($global:counter -ne 0) {
         Write-Host "`nErrors Found" -ForegroundColor Red
         foreach ($error in $global:errorlist) {
-            Write-Host "Error: $error"
+            Write-Host "`t$error" -ForegroundColor Red
         }
     }
 
