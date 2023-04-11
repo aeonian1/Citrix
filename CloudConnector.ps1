8ee124bf007a51fe21526351b4445e689ceb026b
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-11 14:52:22 -0400

    Fixed formatting

--- CloudConnector.ps1
+++ CloudConnector.ps1
@@ -32,92 +32,92 @@ Function Check-ServiceStatus {
             "cdfCaptureService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\CdfCaptureService\CdfCaptureService.exe"
                 Write-Host "Description: Captures CDF traces from all configured components and components"
-                Write-Host "`n"
+                Write-Host ""
              }
             "Citrix Cloud Connector Metrics Service" { 
                 Write-Host "File Location: C:\Program Files(x86)\Citrix\NetScaler Cloud Gateway\MetricsService.exe"
                 Write-Host "Description: Responsible for collection of metrics from the NetScaler Gateway Service as well as for the generation of Synthetics. This service forwards all the data from the Cloud Connector to the Analytics Service"
-                Write-Host "`n"
+                Write-Host ""
             }
             "Citrix NetScaler Cloud Gateway" { 
                 Write-Host "File Location: C:\Program Files(x86)\Citrix\NetScaler Cloud Gateway\Citrix.NetScaler.CloudGateway.exe"
                 Write-Host "Description: Provides internet connectivity to on-premise desktops and applications without the need to open in-bound firewall rules or deploying components in the DMZ"
-                Write-Host "`n"
+                Write-Host ""
             }
             "CitrixClxMtpService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\ClxMtpService\Citrix.ClxMtpService.exe"
                 Write-Host "Description: ClxMtp Service"
-                Write-Host "`n"
+                Write-Host ""
             }
             "CitrixConfigSyncService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\ConfigSync\ConfigSyncService.exe"
                 Write-Host "Description: Copies brokering configuration locally for high availability mode"
-                Write-Host "`n"
+                Write-Host ""
             } 
             "CitrixHighAvailabilityService" { 
                 Write-Host "File Location: C:\Program Files\Citrix\Broker\Service\HighAvailabilityService.exe"
                 Write-Host "Description: Provides continuity of service during outage of central site"
-                Write-Host "`n"
+                Write-Host ""
             }
             "CitrixITSMAdapterProvider" {
                 Write-Host "File Location: C:\Program Files\Citrix\CitrixITSMAdapterProvider\WorkspaceAutomationConnectorPlugin.exe"
                 Write-Host "Description: Automate provisioning and management of Virtual Apps and Desktops"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWEMAuthSvc" {
                 Write-Host "File Location: C:\Program Files\Citrix\WemProvider\Connector.Authentication.Host.exe"
                 Write-Host "Description: Provides authentication service for Citrix WEM Agents to connect to cloud infrastructure servers"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWemMsgSvc" {
                 Write-Host "File Location: C:\Program Files\Citrix\WemProvider\Connector.Messaging.Host.exe"
                 Write-Host "Description: Provides service for Citrix WEM Cloud service to receive messages from cloud infrastructure servers"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudADProvider" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\Agent\Citrix.CloudServices.Agent.exe"
                 Write-Host "Description: Enabled the Citrix Cloud to facilitate management of resources associated with the Active Directory domain accounts it is installed into"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudAgentDiscovery" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\Agent Discovery\Citrix.CloudServices.AgentDiscovery.exe"
                 Write-Host "Description: Enables the Cloud to faciliate management of XenApp and XenDesktop (Legacy)"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudAgentLogger" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\AgentLogger\Citrix.CloudServices.AgentLogger.exe"
                 Write-Host "Description: Enables the Cloud to faciliate management of XenApp and XenDesktop (Legacy)"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudAgentSystem" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\AgentSystem\Citrix.CloudServices.AgentSystem.exe"
                 Write-Host "Description: Handles the system calls necessary for the on-premise agents"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudAgentWatchdog" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\AgentWatchDog\Citrix.CloudServices.AgentWatchDog.exe"
                 Write-Host "Description: Monitors and upgrades the on-premise agents"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudCredentialProvider" {
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\CredentialProvider\Citrix.CloudServices.CredentialProvider.exe"
                 Write-Host "Description: Citrix Cloud credential provider"
-                Write-Host "`n"
+                Write-Host ""
              }
             "CitrixWorkspaceCloudWebRelayProvider" { 
                 Write-Host "File Location: C:\Program Files\Citrix\CloudServices\WebRelayAgent\Citrix.CloudServices.WebRelay.Agent.exe"
                 Write-Host "Description: Enables HTTP Requests received from WebRelay Cloud service to be forwarded to on-premises Web Servers"
-                Write-Host "`n"
+                Write-Host ""
             }
             "RemoteHCLServer" {
                 Write-Host "File Location: C:\Program Files\Citrix\RemoteHCLServer\Service\RemoteHCLServer.exe"
                 Write-Host "Description: Proxies communication between the Delivery Controller and the Hypervisor"
-                Write-Host "`n"
+                Write-Host ""
              }
             "XaXdCloudProxy" { 
                 Write-Host "File Location: C:\Program Files\Citrix\XaXdCloudProxy\XaXdCloudProxy.exe"
                 Write-Host "Description: Enables communication to a remote Broker service from local VDAs and StoreFront servers"
-                Write-Host "`n"
+                Write-Host ""
             }
             Default {
                 Write-Host "Something went wrong"
