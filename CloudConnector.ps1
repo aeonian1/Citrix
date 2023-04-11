f88f28ff730227cd6bd76206d42eed03017b64a2
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-11 12:51:28 -0400

    Added registry info

--- CloudConnector.ps1
+++ CloudConnector.ps1
@@ -210,20 +210,45 @@ function RegistryOutput {
         [Parameter(Mandatory=$true)]
         [String]$Name,
         [Parameter(Mandatory=$true)]
-        [String]$ExpectedValue
+        [String]$ExpectedValue,
+        [Parameter(Mandatory=$true)]
+        [String]$Title
     )
 
-    $LeftSpaced = '{0, -50}' -f $Name + " - "
+    $LeftSpaced = '{0, -50}' -f $Title + " - "
 
-    Write-Host $Name - Get-ItemPropertyValue -Path $Path -Name $Name
-    if (Get-ItemPropertyValue -Path $Path -Name $Name == $ExpectedValue) {
-        Write-Host $LeftSpaced Get-ItemPropertyValue -Path $Path -Name $Name -ForegroundColor Green
+    if ((Get-ItemPropertyValue -Path $Path -Name $Name) -eq $ExpectedValue) {
+        Write-Host $LeftSpaced (Get-ItemPropertyValue -Path $Path -Name $Name) -ForegroundColor Green
     } else {
-        Write-Host $LeftSpaced Get-ItemPropertyValue -Path $Path -Name $Name - "Expected Value = " $ExpectedValue -ForegroundColor Red
+        Write-Host $LeftSpaced (Get-ItemPropertyValue -Path $Path -Name $Name) - "Expected Value = " $ExpectedValue -ForegroundColor Red
     }
 }
 
 
 # Registry values
 
-RegistryOutput -Path HKLM:\Software\Citrix\Broker\Service\LHC -Name Enabled -ExpectedValue 1
+Write-Host "`n# LHC Status"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\Service\LHC" -Name "Enabled" -ExpectedValue 1 -Title "LHC Enabled"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\Service\LHC" -Name "IsElected" -ExpectedValue 1 -Title "LHC Elected Node"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\Service\LHC" -Name "OutageModeEntered" -ExpectedValue 1 -Title "LHC In Outage Mode"
+
+
+Write-Host "`n# Agent Status"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\CloudServices\Install\AgentSystem" -Name "ProductVersionBase" -ExpectedValue "6.70.0" -Title "Agent Version"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\CloudServices\AgentFoundation" -Name "Configured" -ExpectedValue 1 -Title "Agent Configured"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\CloudServices\AgentFoundation" -Name "ImmediateUpgrade" -ExpectedValue 0 -Title "Agent Upgrade Pending"
+RegistryOutput -Path "HKLM:\Software\Citrix\Broker\CloudServices\AgentFoundation" -Name "InMaintenance" -ExpectedValue 0 -Title "Agent in Maintenance"
+
+
+
+Write-Host "`n# Ping Test"
+if (Test-Connection (Get-ItemPropertyValue -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormat")) {
+    RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormat" -ExpectedValue "https://service-us.citrixworkspaceapi.net/" -Title "Connection Status"
+} else {
+    RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormat" -ExpectedValue "https://service-us.citrixworkspaceapi.net/" -Title "Connection Status"
+}
+if (Test-Connection (Get-ItemPropertyValue -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormat")) {
+    RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormatGlobal" -ExpectedValue "https://service.citrixworkspaceapi.net/" -Title "Connection Status Global"
+} else {
+    RegistryOutput -Path "HKLM:\Software\Citrix\CloudServices\AgentFoundation" -Name "ServiceAddressFormatGlobal" -ExpectedValue "https://service.citrixworkspaceapi.net/" -Title "Connection Status Global"
+}
