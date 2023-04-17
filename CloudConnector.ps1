21f55b4311c0005126a8c112957f53f175835699
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-17 10:40:13 -0400

    Updated

--- CloudConnector.ps1
+++ CloudConnector.ps1
@@ -215,7 +215,14 @@ function pingCheck {
 function getEvents {
     $yesterday = (Get-Date) - (New-TimeSpan -Days 1)
     Write-Host "`n--- Citrix Application Events - FATAL / ERROR / WARNING ---" -ForegroundColor Yellow
-    try { Get-WinEvent -FilterHashtable @{LogName='Application'; ProviderName='Citrix*'; Level=1,2,3; StartTime=$yesterday} -ErrorAction Ignore | Format-List } catch { Write-Host "No Events Found" }
+    try { 
+        $event = Get-WinEvent -FilterHashtable @{LogName='Application'; ProviderName='Citrix*'; Level=1,2,3; StartTime=$yesterday} -ErrorAction Ignore  
+        if ($event -eq $null) {
+            Write-Host "No Events Found"
+        } else {
+            Write-Host $event | Format-List
+        }
+    } catch { Write-Host "No Events Found" }
     
 }
 
