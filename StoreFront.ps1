52ddbf15ccabcc880df221d8629f64aeb9a440e4
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-18 14:21:00 -0400

    IIS info add

--- StoreFront.ps1
+++ StoreFront.ps1
@@ -193,11 +193,37 @@ function getEvents {
     } catch { Write-Host "No Events Found" }
 }
 
+function iisInfo {
+    # Get IIS status
+    $iisStatus = Get-Service W3SVC | Select-Object -ExpandProperty Status
+
+    # Get IIS version
+    $iisVersion = (Get-WebConfigurationProperty -filter /system.webServer/serverRuntime -name "version").Value
+
+    # Get IIS application pool information
+    $appPools = Get-ChildItem IIS:\AppPools | Select-Object Name, State, ManagedRuntimeVersion
+
+    # Format output
+    $output = @{
+        "IIS Status" = $iisStatus
+        "IIS Version" = $iisVersion
+        "Application Pools" = $appPools
+    }
+
+    $output.GetEnumerator() | ForEach-Object {
+        $var = $_.Key
+        $value = $_.Value
+        "$var".PadRight(50) + " - $value"
+    }
+}
+
+
 # Entry point into this script
 function main {
     systemInfo
     serviceCheck
     configReplicationStatus
+    iisInfo
 
 
     # Missing DNS entries 
