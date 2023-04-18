2495078d290fda6b0c5d12d153dfabed10b136b1
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-04-18 13:55:29 -0400

    Added another drive

--- CloudConnector.ps1
+++ CloudConnector.ps1
@@ -132,16 +132,20 @@ function systemInfo {
     $diskSize = [math]::Round($disk.Size / 1GB)
     $diskFree = [math]::Round($disk.FreeSpace / 1GB)
     $diskPercentFree = [math]::Round(($disk.FreeSpace / $disk.Size) * 100)
+
     $cpuCores = (Get-WmiObject -Class Win32_Processor).NumberOfCores
     $cpuUsage = Get-WmiObject -Class Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select-Object -ExpandProperty Average
+
     $memory = Get-WmiObject -Class Win32_ComputerSystem | Select-Object TotalPhysicalMemory
     $memorySize = [math]::Round($memory.TotalPhysicalMemory / 1GB)
     $memoryUsage = Get-WmiObject -Class Win32_OperatingSystem | Select-Object FreePhysicalMemory,TotalVisibleMemorySize | ForEach-Object {[math]::Round((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory) / $_.TotalVisibleMemorySize) * 100)}
+
+
     $swap = Get-WmiObject -Class Win32_PageFileUsage | Select-Object AllocatedBaseSize,CurrentUsage
     $swapSize = [math]::Round($swap.AllocatedBaseSize / 1GB)
     $swapUsage = [math]::Round($swap.CurrentUsage / 1GB)
 
-    Write-Host "DISK                                               -  $diskFree GB / $diskSize GB ($diskPercentFree%)"
+    Write-Host "DISK C:                                            -  $diskFree GB / $diskSize GB ($diskPercentFree%)"
     Write-Host "CPU                                                -  $cpuUsage% ($cpuCores cores)"
     Write-Host "MEMORY                                             -  $memoryUsage% ($memorySize GB)"
     Write-Host "SWAP                                               -  $swapUsage GB / $swapSize GB"
