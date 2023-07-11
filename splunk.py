dcf9f1231e03e42a6ef1eea97aa86434c1132db5
aeonian1 <98348103+aeonian1@users.noreply.github.com>
2023-07-11 12:05:26 -0400

    adding

---
+++ splunk.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+#
+# Script to iterate through system logs and get events newer than last record gathered 
+
+import requests
+
+# get records never than utcTimestamp 
+records_date_newer_than = '2023-02-07T15:53:41.9653884Z'
+
+TOKEN_URL = 'https://api-us.cloud.com/cctrustoauth2/root/tokens/clients'
+
+# Obtain bearer token from authorization server
+response = requests.post(TOKEN_URL, data={
+  'grant_type': 'client_credentials',
+  'client_id': '',
+  'client_secret': ''
+})
+print(response)
+token = response.json()
+
+# Create a session object with the relevant HTTP headers
+session = requests.Session()
+session.headers.update({
+  'Authorization': f'CwsAuth Bearer={token["access_token"]}',
+  'Citrix-CustomerId': '625eza6aj2xr',
+  'Accept': 'application/json'
+})
+
+# Get a list of machines in the specified catalog
+response = session.get(f'https://api-us.cloud.com/systemlog/records')
+response.raise_for_status()
+systemlogs = response.json()
+
+for systemlog in systemlogs['items']:
+  if (systemlog['utcTimestamp'] > records_date_newer_than ):
+    print(systemlog)
